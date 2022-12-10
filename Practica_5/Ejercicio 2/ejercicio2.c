ca1f77447994451563e2c40d19447a6f561b8a5f
David Arévalo <72552800+davidarev@users.noreply.github.com>
2022-12-10 17:00:13 +0100

    Add files via upload

---
+++ Practica_5/Ejercicio 2/ejercicio2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    FILE *origen = fopen("origen.txt", "r"); // Creamos y abrimos el archivo origen en modo lectura
+    FILE *destino = fopen("destino.txt", "w"); // Creamos y abrimos el archivo destino en modo escritura
+
+    char linea[100]; // Creamos un buffer para leer el contenido del archivo
+    while (fgets(linea, 100, origen) != NULL) { // Leemos el archivo línea a línea hasta que no queden más líneas
+        fprintf(destino, "%s", linea); // Escribimos la línea leída en el archivo destino
+    }
+
+    fclose(origen); // Cerramos el archivo origen
+    fclose(destino); // Cerramos el archivo destino
+
+    destino = fopen("destino.txt", "r"); // Abrimos el archivo destino en modo lectura
+    while (fgets(linea, 100, destino) != NULL) {
+        printf("%s", linea); // Imprimimos la línea leída del archivo destino
+    }
+    printf("\n");
+    fclose(destino); // Cerramos el archivo destino
+
+    return 0;
+}
